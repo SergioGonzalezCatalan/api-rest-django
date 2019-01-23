7e4a985e1caa1159fca1f109ea6ef14fda2f0508
sergio gonzalez <segonzalez@alemana.cl>
2019-01-23 16:50:56 -0300

    se garegaron funciones POST, PUT PATCH Y DELETE a APIView

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -1,11 +1,15 @@
 # from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from . import serializers
 # Create your views here.
 
 
 class HelloApiView(APIView):
     """Test API View"""
+
+    serializers_class = serializers.HelloSerializer
 
     def get(self, request, format=None):
         """Retorna una lista de caracteristicas de APIView"""
@@ -18,3 +22,30 @@ class HelloApiView(APIView):
         ]
 
         return Response({'message': 'hello', 'an_apiview': an_apiview})
+
+    def post(self, request):
+        """Crea un mensaje de saludo con un nombre"""
+
+        serializer = serializers.HelloSerializer(data=request.data)
+        if serializer.is_valid():
+            name = serializer.data.get('name')
+            message = 'hello {0}'.format(name)
+            return Response({'message': message})
+        else:
+            return Response(serializer.errors, status.HTTP_400_BAD_REQUEST)
+
+    def put(self, request, pk=None):
+        """Maneja la actualizacion de un objeto"""
+
+        return Response({'method': 'put'})
+
+    def patch(self, request, pk=None):
+        """Actualiza campos provisto de un objeto"""
+
+        return Response({'method': 'patch'})
+
+    def delete(self, request, pk=None):
+        """borra un objeto"""
+
+        return Response({'method': 'delete'})
+        
