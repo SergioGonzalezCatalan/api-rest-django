f0a2273f269ac9bb2f316f814bb698516a8ed1e6
sergio gonzalez <segonzalez@alemana.cl>
2019-01-23 18:18:28 -0300

    se agregó filtros de búsqueda a API user profile

--- src/profiles_project/profiles_api/views.py
+++ src/profiles_project/profiles_api/views.py
@@ -4,9 +4,11 @@ from rest_framework.response import Response
 from rest_framework.authentication import TokenAuthentication
 from rest_framework import viewsets
 from rest_framework import status
+from rest_framework import filters
 from . import serializers
 from . import models
 from . import permissions
+
 # Create your views here.
 
 
@@ -110,3 +112,6 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     queryset = models.MyUser.objects.all()
     authentication_classes = (TokenAuthentication,)
     permission_classes = (permissions.UpdateOwnProfile,)
+
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('date_of_birth', 'email',)
